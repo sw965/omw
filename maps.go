5a42819643a3b14db07a3b56bca4b54ad01b36a4
sw965 <none>
2024-05-09 22:06:30 +0900

    機能の追加

--- maps.go
+++ maps.go
@@ -7,3 +7,11 @@ func InvertMap[YM ~map[V]K, XM ~map[K]V, K, V comparable](xm XM) YM {
 	}
 	return ym
 }
+
+func MapValueMapFunc[YM ~map[K]YV, XM ~map[K]XV, K comparable, XV, YV any](xm XM, f func(XV)YV) YM {
+	ym := YM{}
+	for k, v := range xm {
+		ym[k] = f(v)
+	}
+	return ym
+}
